0f4ab7f8d528463fcccc69840c6e9353e09030ea
alanl248 <alanly71103@gmail.com>
2025-11-03 14:50:47 -0800

    M7L7

--- M07.telling_a_story_workbook/dialouge.gd
+++ M07.telling_a_story_workbook/dialouge.gd
@@ -2,7 +2,7 @@ extends Control
 
 @onready var rich_text_label: RichTextLabel = %RichTextLabel
 @onready var next_button: Button =  %NextButton
-
+@onready var audio_stream_player: AudioStreamPlayer = %AudioStreamPlayer
 var dialogue_items :Array[String] = [
 	"Stop taking my chips",
 	"But but I'm just a behbee",
@@ -18,10 +18,18 @@ func _ready()-> void:
 func show_text() -> 	void:
 	var current_item := dialogue_items[current_item_index]
 	rich_text_label.text = current_item
-	
+	rich_text_label.visible_ratio = 0.0
+	var tween := create_tween()
+	var text_appearing_duration := 1.2
+	var sound_max_offset := audio_stream_player.stream.get_length() - text_appearing_duration
+	tween.tween_property(rich_text_label, "visible_ratio",1.0, text_appearing_duration)
+	var sound_start_position := randf() * sound_max_offset
+	audio_stream_player.play(sound_start_position)
+	tween.finished.connect(audio_stream_player.stop)
 func advance() -> void:
 	current_item_index += 1
 	if current_item_index == dialogue_items.size():
 		get_tree().quit()
 	else:
 		show_text()
+		
---
+++ M07.telling_a_story_workbook/expressions.gd
@@ -0,0 +1,9 @@
+extends Control
+
+@onready var body: TextureRect = %Body
+@onready var expression: TextureRect = %Expression
+@onready var button_Sophia: Button = %ButtonSophia
+@onready var button_Pink: Button = %ButtonPink 
+@onready var button_Regular: Button = %ButtonRegular
+@onready var button_Sad: Button = %ButtonSad
+@onready var button_Happy: Button = %ButtonHappy
