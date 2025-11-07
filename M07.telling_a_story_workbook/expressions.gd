f423b8e5b135e9bfe925d094bd2f6f0dec1f6d02
alanl248 <alanly71103@gmail.com>
2025-11-06 21:05:00 -0800

    M7L9

--- M07.telling_a_story_workbook/expressions.gd
+++ M07.telling_a_story_workbook/expressions.gd
@@ -1,9 +1,50 @@
 extends Control
 
+var bodies := {
+	"sophia": preload("res://assets/sophia.png"),
+	"pink": preload("res://assets/pink.png")
+}
+
+var expressions := {
+	"happy": preload("res://assets/emotion_happy.png"),
+	"regular": preload("res://assets/emotion_regular.png"),
+	"sad": preload("res://assets/emotion_sad.png"),
+}
+
 @onready var body: TextureRect = %Body
 @onready var expression: TextureRect = %Expression
-@onready var button_Sophia: Button = %ButtonSophia
-@onready var button_Pink: Button = %ButtonPink 
-@onready var button_Regular: Button = %ButtonRegular
-@onready var button_Sad: Button = %ButtonSad
-@onready var button_Happy: Button = %ButtonHappy
+@onready var row_bodies: HBoxContainer = %RowBodies
+@onready var row_expressions: HBoxContainer = %RowExpressions
+
+
+func _ready() -> void:
+	create_buttons()
+
+
+func create_button_pink() -> void:
+	var button := Button.new()
+	row_bodies.add_child(button)
+
+	var key := "pink"
+	button.text = key.capitalize()
+	button.pressed.connect(func() -> void:
+		body.texture = bodies[key]
+	)
+
+
+func create_buttons() -> void:
+	for current_body: String in bodies:
+		var button := Button.new()
+		row_bodies.add_child(button)
+		button.text = current_body.capitalize()
+		button.pressed.connect(func() -> void:
+			body.texture = bodies[current_body]
+		)
+
+	for current_expression: String in expressions:
+		var button := Button.new()
+		row_expressions.add_child(button)
+		button.text = current_expression.capitalize()
+		button.pressed.connect(func() -> void:
+			expression.texture = expressions[current_expression]
+		)
