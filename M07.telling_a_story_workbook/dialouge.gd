e148b0c1816aa6a466f536c1ba9727f24aabbdac
alanl248 <alanly71103@gmail.com>
2025-11-07 15:25:52 -0800

    M7L11

--- M07.telling_a_story_workbook/dialouge.gd
+++ M07.telling_a_story_workbook/dialouge.gd
@@ -1,26 +1,61 @@
 extends Control
 
-var expressions :={
-"happy":preload("res://assets/emotion_happy.png"),
-"regular":preload("res://assets/emotion_regular.png"),
-"sad":preload("res://assets/emotion_sad.png"),
+var expressions := {
+	"happy": preload ("res://assets/emotion_happy.png"),
+	"regular": preload ("res://assets/emotion_regular.png"),
+	"sad": preload ("res://assets/emotion_sad.png"),
 }
+
+var bodies := {
+	"sophia": preload ("res://assets/sophia.png"),
+	"pink": preload ("res://assets/pink.png")
+}
+
+
 var dialogue_items: Array[Dictionary] = [
 	{
-	"expression":expressions["regular"],
-	"text": "Herro, class",
+		"expression": expressions["regular"],
+		"text": "I've been studying arrays and dictionaries lately.",
+		"character": bodies["sophia"],
 	},
 	{
-	"expression":expressions["sad"],
-	"text":"I want eggrolls",
+		"expression": expressions["regular"],
+		"text": "Oh, nice. How has it been going?",
+		"character": bodies["pink"],
 	},
 	{
-	"expression":expressions["happy"],
-	"text":"ahh, senpai binkowski has more eggrolls",
-	}
+		"expression": expressions["sad"],
+		"text": "Well... it's a little complicated!",
+		"character": bodies["sophia"],
+	},
+	{
+		"expression": expressions["sad"],
+		"text": "Oh!",
+		"character": bodies["pink"],
+	},
+	{
+		"expression": expressions["regular"],
+		"text": "It sure takes time to click at first.",
+		"character": bodies["pink"],
+	},
+	{
+		"expression": expressions["happy"],
+		"text": "If you keep at it, eventually, you'll get the hang of it!",
+		"character": bodies["pink"],
+	},
+	{
+		"expression": expressions["regular"],
+		"text": "Mhhh... I see. I'll keep at it, then.",
+		"character": bodies["sophia"],
+	},
+	{
+		"expression": expressions["happy"],
+		"text": "Thanks for the encouragement. Time to LEARN!!!",
+		"character": bodies["sophia"],
+	},
 ]
-
 var current_item_index := 0
+
 
 @onready var rich_text_label: RichTextLabel = %RichTextLabel
 @onready var next_button: Button = %NextButton
@@ -29,24 +64,32 @@ var current_item_index := 0
 @onready var expression: TextureRect = %Expression
 
 
-
 func _ready() -> void:
 	show_text()
 	next_button.pressed.connect(advance)
 
-## Draws the current text to the rich text element
+
 func show_text() -> void:
+	
 	var current_item := dialogue_items[current_item_index]
 	rich_text_label.text = current_item["text"]
 	expression.texture = current_item["expression"]
+	body.texture = current_item["character"]
+
+	
 	rich_text_label.visible_ratio = 0.0
 	var tween := create_tween()
-	var text_appearing_duration := 1.2
+	var text_appearing_duration: float = current_item["text"].length() / 30.0
 	tween.tween_property(rich_text_label, "visible_ratio", 1.0, text_appearing_duration)
+
+	
 	var sound_max_offset := audio_stream_player.stream.get_length() - text_appearing_duration
 	var sound_start_position := randf() * sound_max_offset
 	audio_stream_player.play(sound_start_position)
 	tween.finished.connect(audio_stream_player.stop)
+
+	slide_in()
+
 
 func advance() -> void:
 	current_item_index += 1
@@ -54,3 +97,13 @@ func advance() -> void:
 		get_tree().quit()
 	else:
 		show_text()
+
+
+## Animates the character sliding in.
+func slide_in() -> void:
+	var slide_tween := create_tween()
+	slide_tween.set_ease(Tween.EASE_OUT)
+	body.position.x = 200
+	slide_tween.tween_property(body, "position:x", 0, 0.3)
+	body.modulate.a = 0
+	slide_tween.parallel().tween_property(body, "modulate:a", 1, 0.2)
